2e7de050fd6d91df007eebc5eed459f9abcdb92b
Gert-Jan <gertjanboth@gmail.com>
2020-01-08 09:58:06 +0100

    Initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    Dummy conftest.py for network_derivs.
+
+    If you don't know what this is for, just leave it empty.
+    Read more about conftest.py under:
+    https://pytest.org/latest/plugins.html
+"""
+
+# import pytest
