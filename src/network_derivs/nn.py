c0daf1f60f9c349ff80c65f9cbe55915a3f1749d
Gert-Jan <gert-jan.both@cri-paris.org>
2020-01-08 09:39:47 +0000

    Linear layer works with stacked derivs

--- src/network_derivs/nn.py
+++ src/network_derivs/nn.py
@@ -2,16 +2,18 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
+# Linear layer
 class Linear(nn.Linear):
     def __init__(self, in_features, out_features, bias=True):
         super().__init__(in_features, out_features, bias)
         
     def forward(self, input):
-        z = [F.linear(input[0], self.weight, self.bias)]
-        dz = [F.linear(dx, self.weight) for dx in input[1:]]
-        return z + dz
+        x, dx = input
+        z = F.linear(x self.weight, self.bias)
+        dz = F.linear(dx, self.weight)
+        return (z, dz)
 
+# Activation function 
 def activation_func_derivs(input, sigma, dsigma, d2sigma, d3sigma):
     df = input[1] * dsigma[:, None, :]
     d2f = input[1]**2 * d2sigma[:, None, :] + input[2] * dsigma[:, None, :]
@@ -19,6 +21,7 @@ def activation_func_derivs(input, sigma, dsigma, d2sigma, d3sigma):
     
     return df, d2f, d3f
 
+# Specific activation function implementations
 class Tanh(nn.Module):
     def __init__(self):
         super().__init__()
