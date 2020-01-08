6278e107d9937bc9cc3f33c8873c2dfc190c637e
Gert-Jan <gert-jan.both@cri-paris.org>
2020-01-08 09:25:33 +0000

    Separated code activation general and specific

--- src/network_derivs/nn_deriv.py
+++ src/network_derivs/nn_deriv.py
@@ -12,6 +12,13 @@ class Linear(nn.Linear):
         dz = [F.linear(dx, self.weight) for dx in input[1:]]
         return z + dz
 
+def activation_func_derivs(input, sigma, dsigma, d2sigma, d3sigma):
+    df = input[1] * dsigma[:, None, :]
+    d2f = input[1]**2 * d2sigma[:, None, :] + input[2] * dsigma[:, None, :]
+    d3f = 3 * input[1] * input[2] * d2sigma[:, None, :] + input[1]**3 * d3sigma[:, None, :] + input[3] * dsigma[:, None, :]        
+    
+    return df, d2f, d3f
+
 class Tanh(nn.Module):
     def __init__(self):
         super().__init__()
@@ -22,8 +29,6 @@ class Tanh(nn.Module):
         d2sigma = -2 * sigma * dsigma
         d3sigma = d2sigma**2 / dsigma - 2*dsigma**2
         
-        df = input[1] * dsigma[:, None, :]
-        d2f = input[1]**2 * d2sigma[:, None, :] + input[2] * dsigma[:, None, :]
-        d3f = 3 * input[1] * input[2] * d2sigma[:, None, :] + input[1]**3 * d3sigma[:, None, :] + input[3] * dsigma[:, None, :]        
+        df, d2f, d3f = activation_func_derivs(input, sigma, dsigma, d2sigma, d3sigma)
         
         return [sigma, df, d2f, d3f]
